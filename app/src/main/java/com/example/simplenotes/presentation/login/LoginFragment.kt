624d4cf677944b405de50a2d269e7ab0b3cc9d93
Sergey Tesalovskiy <tesser.99.2012@gmail.com>
2021-02-10 23:38:49 +0300

    text field listeners in LoginFragment.kt and navigation to sign up

--- app/src/main/java/com/example/simplenotes/presentation/login/LoginFragment.kt
+++ app/src/main/java/com/example/simplenotes/presentation/login/LoginFragment.kt
@@ -1,18 +1,55 @@
 package com.example.simplenotes.presentation.login
 
 import android.os.Bundle
-import android.view.View
+import android.util.Log
+import android.view.LayoutInflater
 import androidx.fragment.app.Fragment
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.widget.doOnTextChanged
+import androidx.navigation.fragment.findNavController
 import com.example.simplenotes.R
+import com.example.simplenotes.databinding.FragmentLoginBinding
 
-class LoginFragment : Fragment(R.layout.fragment_login) {
+class LoginFragment : Fragment() {
+
+    private val binding: FragmentLoginBinding by lazy {
+        FragmentLoginBinding.inflate(layoutInflater)
+    }
+
+    private var email: String = ""
+    private var password: String = ""
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = binding.root
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-    }
 
+        binding.email.editText?.doOnTextChanged { text, start, before, count ->
+            binding.email.error = if (count == 0) getString(R.string.cannot_be_empty) else null
+            email = text.toString()
+        }
+
+        binding.password.editText?.doOnTextChanged { text, start, before, count ->
+            binding.password.error = if (count == 0) getString(R.string.cannot_be_empty) else null
+            password = text.toString()
+        }
+
+        binding.signin.setOnClickListener {
+            if(email.isNotBlank() && password.isNotBlank()){
+                // start login flow
+            }
+
+        }
+
+        binding.signup.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_signUpFragment)
+        }
+    }
     //переход с фрагмента Splash на LoginActivity
     //findNavController().navigate(R.id.action_loginFragment_to_mainActivity)
-
-
 }
