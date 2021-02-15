8827df5a096a2b628d6ef50274b5c534dcb6b0a0
Sergey Tesalovskiy <tesser.99.2012@gmail.com>
2021-02-15 19:08:40 +0300

    empty repository and use cases

--- app/src/main/java/com/example/simplenotes/domain/repositories/IRepository.kt
+++ app/src/main/java/com/example/simplenotes/domain/repositories/IRepository.kt
@@ -1,8 +1,21 @@
 package com.example.simplenotes.domain.repositories
 
 import com.google.firebase.auth.AuthResult
+import com.google.firebase.firestore.DocumentSnapshot
+import com.google.firebase.firestore.QuerySnapshot
 
 interface IRepository {
+
+    interface FirestoreRepository{
+        suspend fun getAllTasksByUser(userId: String): QuerySnapshot
+
+        //Тип для параметра task нужно изменить
+        suspend fun addNewTask(userId: String, task: Boolean = false): Boolean
+
+        suspend fun getTasksByCategoryId(userId: String, category: String): QuerySnapshot
+        suspend fun createCategory(userId: String, name: String): Boolean
+        suspend fun getAllCategoriesByUser(userId: String): QuerySnapshot
+    }
 
     interface AuthRepository{
         suspend fun signIn(email: String, password: String): AuthResult
