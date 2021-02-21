ea13899af5a4d8a7d117e7fb078ce077420d25f8
Sergey Tesalovskiy <tesser.99.2012@gmail.com>
2021-02-21 14:46:03 +0300

    get all tasks and categories repository methods, use cases

--- app/src/main/java/com/example/simplenotes/domain/usecases/GetAllTasksByUserUseCase.kt
+++ app/src/main/java/com/example/simplenotes/domain/usecases/GetAllTasksByUserUseCase.kt
@@ -3,5 +3,5 @@ package com.example.simplenotes.domain.usecases
 import com.example.simplenotes.domain.repositories.IRepository
 
 class GetAllTasksByUserUseCase(private val firestoreRepository: IRepository.FirestoreRepository) {
-    suspend fun execute(userId: String) = firestoreRepository.getAllTasks()
+    suspend fun execute() = firestoreRepository.getAllTasks()
 }
