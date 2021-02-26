308585fc965f1c53a962bc35f81fb85d223f8c96
vladimir <holdencolfild19@gmail.com>
2021-02-26 17:43:30 +0300

    Main Screen Commit(исправил косяки при слиянии)

--- app/src/main/java/com/example/simplenotes/data/repositories/FirestoreRepository.kt
+++ app/src/main/java/com/example/simplenotes/data/repositories/FirestoreRepository.kt
@@ -1,6 +1,8 @@
 package com.example.simplenotes.data.repositories
 
+
 import com.example.simplenotes.domain.entities.Category
+import android.util.Log
 import com.example.simplenotes.domain.entities.Task
 import com.example.simplenotes.domain.repositories.IRepository
 import com.google.firebase.auth.ktx.auth
@@ -25,25 +27,41 @@ class FirestoreRepository : IRepository.FirestoreRepository {
         category.id = doc.id
 
         return userId?.let { block ->
-            db.collection(COLLECTION_USERS).document(block).collection(COLLECTION_CATEGORIES).document(category.id)
-                    .set(category)
-                    .isSuccessful
+            db.collection(COLLECTION_USERS).document(block).collection(COLLECTION_CATEGORIES)
+                .document(category.id)
+                .set(category)
+                .isSuccessful
         } ?: false
     }
 
     override suspend fun getAllCategories() = userId?.let { userId ->
-        db.collection(COLLECTION_USERS).document(userId).collection(COLLECTION_CATEGORIES).get().await()
+        db.collection(COLLECTION_USERS).document(userId).collection(COLLECTION_CATEGORIES).get()
+            .await()
     }
 
     override suspend fun getAllTasks() = userId?.let { userId ->
-        db.collection(COLLECTION_USERS).document(userId).collection(COLLECTION_NOTES).get().await()
+        db.collection(COLLECTION_USERS).document(userId).collection(COLLECTION_NOTES).get()
+            .await()
     }
 
     override suspend fun addNewTask(task: Task): Boolean {
-        TODO("Not yet implemented")
+        val doc = db.collection(COLLECTION_USERS).document()
+        task.id = doc.id
+
+        return userId?.let { it1 ->
+            db.collection(COLLECTION_USERS).document(it1).collection(COLLECTION_NOTES)
+                .document(task.id)
+                .set(task)
+                .isSuccessful
+        } ?: false
     }
 
-    override suspend fun getTasksByCategoryId(category: String): QuerySnapshot {
-        TODO("Not yet implemented")
+    override suspend fun getTasksByCategoryId(category: String): QuerySnapshot? {
+        return userId?.let { userId ->
+            db.collection(COLLECTION_USERS).document(userId).collection(COLLECTION_NOTES)
+                .whereEqualTo("category", category).get().await()
+        }
+
     }
+
 }
