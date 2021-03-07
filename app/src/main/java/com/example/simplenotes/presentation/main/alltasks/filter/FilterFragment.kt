46f8d611addfe02457277ee6afc7686f65b90137
Sergey Tesalovskiy <tesser.99.2012@gmail.com>
2021-03-07 22:16:41 +0300

    default categories displaying in chips

--- app/src/main/java/com/example/simplenotes/presentation/main/alltasks/filter/FilterFragment.kt
+++ app/src/main/java/com/example/simplenotes/presentation/main/alltasks/filter/FilterFragment.kt
@@ -1,12 +1,17 @@
 package com.example.simplenotes.presentation.main.alltasks.filter
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.forEach
+import androidx.fragment.app.viewModels
 import com.example.simplenotes.R
 import com.example.simplenotes.databinding.FragmentFilterBinding
+import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipDrawable
 
 
 class FilterFragment : Fragment() {
@@ -18,6 +23,8 @@ class FilterFragment : Fragment() {
     private val filterOptions: FilterOptions? by lazy {
         FilterFragmentArgs.fromBundle(requireArguments()).filterOptions
     }
+
+    private val viewModel by viewModels<FilterViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -32,10 +39,22 @@ class FilterFragment : Fragment() {
         filterOptions?.let {
             initViewWithReceivedFilter(it)
         } ?: initViewWithDefaultFilter()
+
+
+        viewModel.allCategories.observe(viewLifecycleOwner) { allCategories ->
+            allCategories.forEach {
+                val chip = layoutInflater.inflate(R.layout.checked_chip, binding.categories, false) as Chip
+                chip.text = it.name
+                chip.setOnCheckedChangeListener { buttonView, isChecked ->
+                    Log.d("KEK", "chip with text ${buttonView.text} check status is $isChecked")
+                }
+                binding.categories.addView(chip)
+            }
+        }
     }
 
     private fun initViewWithDefaultFilter() {
-
+        viewModel.defaults()
     }
 
     private fun initViewWithReceivedFilter(filterOptions: FilterOptions) {
--- app/src/main/java/com/example/simplenotes/presentation/main/alltasks/filter/FilterViewModel.kt
+++ app/src/main/java/com/example/simplenotes/presentation/main/alltasks/filter/FilterViewModel.kt
@@ -1,4 +1,39 @@
 package com.example.simplenotes.presentation.main.alltasks.filter
 
-class FilterViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.simplenotes.data.repositories.FirestoreRepository
+import com.example.simplenotes.data.repositories.RemoteRepository
+import com.example.simplenotes.domain.entities.Category
+import com.example.simplenotes.domain.entities.Task
+import com.example.simplenotes.domain.usecases.GetAllCategoriesByUser
+import com.google.firebase.firestore.ktx.toObject
+import kotlinx.coroutines.launch
+
+class FilterViewModel: ViewModel() {
+    private val _allCategories = MutableLiveData<List<Category>>()
+    val allCategories: LiveData<List<Category>>
+        get() = _allCategories
+
+    private val _checkedCategories = MutableLiveData<List<Category>>()
+    val checkedCategories: LiveData<List<Category>>
+        get() = _checkedCategories
+
+    fun defaults(){
+        viewModelScope.launch {
+            GetAllCategoriesByUser(FirestoreRepository()).execute()?.let { querySnapshot ->
+                val result = mutableListOf<Category>()
+
+                querySnapshot.forEach {
+                    result.add(it.toObject(Category::class.java))
+                }
+
+                _allCategories.postValue(result.toList())
+                _checkedCategories.postValue(result.toList())
+            }
+        }
+    }
+
 }
